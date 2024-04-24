7c16ca06f40c2fef4ecea08c11f74499b0509fca
Nguyen Thi Lan Anh <nguyenlananh25112003@gmail.com>
2024-04-24 16:59:26 +0700

    error

--- app/src/main/java/com/example/expensemanager/HomeActivity.java
+++ app/src/main/java/com/example/expensemanager/HomeActivity.java
@@ -1,23 +1,92 @@
 package com.example.expensemanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // Ẩn thanh tiêu đề
-        getSupportActionBar().hide();
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Expense Manager");
+        setSupportActionBar(toolbar);
 
-        // Hiển thị thanh thời gian và pin nhỏ
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
+
+//        // Ẩn thanh tiêu đề
+//        getSupportActionBar().hide();
+//
+//        // Hiển thị thanh thời gian và pin nhỏ
+//        View decorView = getWindow().getDecorView();
+//        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout =findViewById(R.id.drawer_layout);
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.END)){
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }else{
+            super.onBackPressed();
+        }
+    }
+    public void displaySelectedListener(int itemId){
+
+        Fragment fragment = null;
+
+        if (itemId == R.id.dashboard) {
+            // Xử lý khi chọn dashboard
+        } else if (itemId == R.id.income) {
+            // Xử lý khi chọn income
+        } else if (itemId == R.id.expense) {
+            // Xử lý khi chọn expense
+        } else {
+            // Xử lý trường hợp không khớp
+        }
+        if (fragment != null){
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.main_frame, fragment);
+            ft.commit();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        displaySelectedListener(item.getItemId());
+        return true;
     }
 }
